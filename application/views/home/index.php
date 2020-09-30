faa1e526e309792be524db06c2923aadf64c0433
bowoid <69063129+bowoid@users.noreply.github.com>
2020-10-01 04:26:36 +0700

    upload
    simple ci

---
+++ application/views/home/index.php
@@ -0,0 +1,11 @@
+
+    <h1>Hello Word, <?= $nama; ?>!</h1>
+</div>
+<div class="sm-span4 smue-clmn sme-dsbl-margin-left sme-dsbl-margin-right">
+<div class="smue-image-obj smue-text-align-center"><a href="images/62-856-2566-334-20190810_135052-1.jpg" data-action="smueLightbox" target="_self"><img src="http://bwo.web.id/images/62-856-2566-334-20190810_135052-1.jpg" title="+62 856-2566-334 20190810_135052" alt="+62 856-2566-334 20190810_135052" class="smue-image-obj-basic sme-dsbl-margin-left sme-dsbl-margin-right sme-dsbl-margin-top sme-dsbl-margin-bottom" /></a></div>
+</div>
+<div class="sm-span4 smue-clmn sme-dsbl-margin-left sme-dsbl-margin-right">
+<div class="smue-text-obj">
+<h2 class="tm-section-title tm-section-title-box tm-box-bg-title">bowoid 2020 </h2>
+</div>
+ 
